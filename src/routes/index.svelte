a1f9ec8a6f632aa1cc77e85fdc448f901b9cadc2
azukisiromochi <azukisiromochi@gmail.com>
2019-06-24 20:07:04 +0900

    add: 雑にモーダル表示するところまで

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,9 @@
+<script>
+	import Modal from '../components/Modal.svelte';
+
+	let showModal = false;
+</script>
+
 <style lang="scss">
 	$gradient: linear-gradient(-90deg, #FF00A1, #F6FF00);
 
@@ -47,3 +53,27 @@
 </figure>
 
 <p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+
+<button on:click="{() => showModal = true}">
+	show modal
+</button>
+
+{#if showModal}
+	<Modal on:close="{() => showModal = false}">
+		<h2 slot="header">
+			modal
+			<small><em>adjective</em>  mod·al \ˈmō-dəl\</small>
+		</h2>
+
+		<ol class="definition-list">
+			<li>of or relating to modality in logic</li>
+			<li>containing provisions as to the mode of procedure or the manner of taking effect —used of a contract or legacy</li>
+			<li>of or relating to a musical mode</li>
+			<li>of or relating to structure as opposed to substance</li>
+			<li>of, relating to, or constituting a grammatical form or category characteristically indicating predication</li>
+			<li>of or relating to a statistical mode</li>
+		</ol>
+
+		<a href="https://www.merriam-webster.com/dictionary/modal">merriam-webster.com</a>
+	</Modal>
+{/if}
